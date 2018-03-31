785bbf5b53e858e228083578486e37f3b29ffa49
Keith Holliday <keithrholliday@gmail.com>
2018-03-30 21:21:26 -0500

    Made h1 responsive

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -171,7 +171,7 @@ export default {
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
   h1 {
-      font-size: 100px;
+      font-size: 10vw;
   }
 
   .Approved h1 {
